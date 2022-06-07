6392ff8a9e1386951a4a1154d24b99c26183bc0f
Ian Dao <ibdao921@gmail.com>
2022-06-07 15:14:43 -0700

    serial generator done

--- serial.py
+++ serial.py
@@ -17,3 +17,38 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start): # start = 100
+        """Sets the starting point for the generator
+            start will equal the given starting point.
+            count will begin at 0.
+        """
+
+        self.start = start
+        self.count = 0
+
+    def reset(self):
+        """Resets the generator back to given start
+            sets count back to 0 and returns `None`
+        """
+
+        self.count = 0
+        return None
+
+    def generate(self):
+        """Generates next sequential number
+            returns the starting point plus count
+            and increments count by 1.
+        """
+
+        current = self.start + self.count #current = 100 + 0
+        self.count +=1 #self.count = 1
+        return current
+    
+        
+        
+
+        
+        
+
+
+    
